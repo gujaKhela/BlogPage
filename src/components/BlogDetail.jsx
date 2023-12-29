623a9739381e9637ef375665e15fbb3c560bc9ca
gujaKhela <khelashviliguja@gmail.com>
2023-12-29 17:53:00 +0400

    working on small bugs

--- src/components/BlogDetail.jsx
+++ src/components/BlogDetail.jsx
@@ -1,23 +1,23 @@
 // BlogDetails.jsx
 import { useAuth } from "../contexts/AuthContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logoImage from "../../src/img/LOGO.png";
-import blogImage from "../img/Blog.png"
+import blogImage from "../img/Blog.png";
 
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchData } from "../api/fetchData"; // Adjust the import path
 import SimilarComponents from "./SimilarComponents";
+
 import LoginModal from "./LoginModal";
 
-
 const BlogDetails = () => {
+  const navigate = useNavigate(); // Use useNavigate to navigate to different routes
+
   const { id } = useParams();
   const [blogDetail, setBlogDetail] = useState(null);
   const { isLoggedIn, logout } = useAuth();
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-
-
 
   const openLoginModal = () => {
     setIsLoginModalOpen(true);
@@ -63,47 +63,35 @@ const BlogDetails = () => {
 
   return (
     <>
-      
-
-
       <header className="">
-      <div className="flex justify-between items-center w-11/12 mx-auto">
-        <Link to="/" className="text-2xl font-bold">
-          <img src={logoImage} alt="Logo" className="h-8" />
-        </Link>
+        <div className="flex justify-between items-center w-11/12 mx-auto">
+          <Link to="/" className="text-2xl font-bold">
+            <img src={logoImage} alt="Logo" className="h-8" />
+          </Link>
 
-        {isLoggedIn ? (
-          <button
-            className="bg-indigo-600 text-white px-4 py-2 rounded"
-            onClick={handleAddBlogClick}
-          >
-            დაამატე ბლოგი
-          </button>
-        ) : (
-          <button
-            className="bg-indigo-600 text-white px-4 py-2 rounded"
-            onClick={openLoginModal}
-          >
-            შესვლა
-          </button>
-        )}
-      </div>
-      <LoginModal isOpen={isLoginModalOpen} onClose={closeLoginModal} />
-      <div className="flex justify-between items-center mt-20 w-11/12 border-b mx-auto ">
-            <p className="font-bold text-6xl text-black"> ბლოგი </p>
-            <img src={blogImage} alt="Blog" />
-          </div>
+          {isLoggedIn ? (
+            <button
+              className="bg-indigo-600 text-white px-4 py-2 rounded"
+              onClick={handleAddBlogClick}
+            >
+              დაამატე ბლოგი
+            </button>
+          ) : (
+            <button
+              className="bg-indigo-600 text-white px-4 py-2 rounded"
+              onClick={openLoginModal}
+            >
+              შესვლა
+            </button>
+          )}
+        </div>
+        <LoginModal isOpen={isLoginModalOpen} onClose={closeLoginModal} />
+        <div className="flex justify-between items-center mt-20 w-11/12 border-b mx-auto ">
+          <p className="font-bold text-6xl text-black"> ბლოგი </p>
+          <img src={blogImage} alt="Blog" />
+        </div>
       </header>
 
-
-
-
-
-
-
-
-
-      
       <div
         style={{
           width: "720px",
